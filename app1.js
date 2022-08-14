773e0d90242a9491adafd0953aa9e0c70b7ea16a
hrk-git <78955872+hrk-git@users.noreply.github.com>
2022-08-14 10:14:51 -0700

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,26 @@
+function getHours(){
+
+    const now=new Date();
+    const hours=now.getHours();
+
+   document.write(hours);
+
+}
+function getMinutes(){
+
+const now=new Date();
+const minutes=now.getMinutes();
+
+
+document.write(minutes);
+
+}
+function getSeconds(){
+
+const now=new Date();
+
+const seconds=now.getSeconds();
+
+document.write(seconds);
+
+}
